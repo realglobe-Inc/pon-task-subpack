8956bd7f194417825559351751b9b7a80bd29787
okunishinishi <okunishinishi@gmail.com>
2019-05-27 13:42:34 +0900

    remove shim

---
+++ test/sync_test.js
@@ -0,0 +1,26 @@
+/**
+ * Test case for sync.
+ * Runs with mocha.
+ */
+'use strict'
+
+const sync = require('../lib/sync.js')
+const assert = require('assert')
+
+describe('sync', function () {
+  this.timeout(3000)
+
+  before(async () => {
+
+  })
+
+  after(async () => {
+
+  })
+
+  it('Sync', async () => {
+
+  })
+})
+
+/* global describe, before, after, it */
